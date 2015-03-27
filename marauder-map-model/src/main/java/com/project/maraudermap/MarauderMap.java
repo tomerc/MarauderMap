318b0b580148955dfd366b2b507a9ef92ad1fcf0
Tomer Cohen <ilan012@gmail.com>
2015-03-26 20:48:27 -0400

    initial commit of model module

---
+++ marauder-map-model/src/main/java/com/project/maraudermap/MarauderMap.java
@@ -0,0 +1,10 @@
+package com.project.maraudermap;
+
+import java.io.Serializable;
+
+/**
+ * @author Tomer Cohen
+ */
+public class MarauderMap implements Serializable {
+    private static final long serialVersionUID = -7963528983093496518L;
+}
